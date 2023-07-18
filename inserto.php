fd81053ef411f1ab8b0e51412d8672b211c42826
jgarcia <jgarcia@newemage.com>
2023-07-18 16:52:33 -0600

    Add files via upload

--- inserto.php
+++ inserto.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Inserto 
  * Description: Allows you to insert code or text in the header or footer
- * Version: 2.5.9
+ * Version: 2.6.1
  * Author: Newemage
  * Author URI: https://newemage.com
  * License: GPLv2 or later
@@ -45,9 +45,9 @@ if ( !class_exists( 'Inserto' ) ) {
 
 		function admin_init() {
 
-			register_setting( 'header-and-footer-scripts', 'inserto_admin', 'trim' );
-			register_setting( 'header-and-footer-scripts', 'inserto_header', 'trim' );
-			register_setting( 'header-and-footer-scripts', 'inserto_footer', 'trim' );
+			register_setting( 'header-and-footer-scripts', 'inserto_admin', 'base64_encode' );
+			register_setting( 'header-and-footer-scripts', 'inserto_header', 'base64_encode' );
+			register_setting( 'header-and-footer-scripts', 'inserto_footer', 'base64_encode' );
 
 		
 			foreach ( get_post_types( '', 'names' ) as $type ) {
@@ -57,11 +57,17 @@ if ( !class_exists( 'Inserto' ) ) {
 			add_action('save_post','inserto_meta_save');
 
 			add_shortcode( 'inserto', function(){
-				$text = get_option( 'inserto_short', '' );
+				$text = is64(get_option( 'inserto_short', '' ));
 				$text = do_shortcode( $text );
 
 				echo $text."\n";
 			} );
+
+			add_shortcode('credits',function($a){
+				$a['copy'] = isset($a['copy']) ? : 'Todos los derechos reservados'; 
+				$a['msj'] = isset($a['msj']) ? : 'Diseño web por'; 
+				return date('Y')."© {$a['copy']} &mdash; {$a['msj']} <a href=https://newemage.com.mx target=_b>Newemage</a>";
+		  });
 		}
 
 		function admin_menu() {
@@ -70,41 +76,59 @@ if ( !class_exists( 'Inserto' ) ) {
 			}
 
 		function wp_head() {
-			$meta = get_option( 'inserto_header', '' );
-			if ( $meta != '' ) 
-				echo $meta."\n";
+			$text = is64(get_option( 'inserto_header', '' ));
+			if ( $text != '' ) 
+				echo "\n\n<!--◢◤◢◤◢◤◢◤◢◤◢◤◢◤  START OF HEAD  ◢◤◢◤◢◤◢◤◢◤◢◤◢◤-->\n\n {$text} \n\n<!--◢◤◢◤◢◤◢◤◢◤◢◤◢◤  END OF HEAD  ◢◤◢◤◢◤◢◤◢◤◢◤◢◤-->\n\n";
 			
 
 		}
 
 		function wp_footer() {
 
-			$text = get_option( 'inserto_footer', '' );
+			$text = is64(get_option( 'inserto_footer', '' ));
 			if ( $text != '' && !is_admin() && !is_feed() && !is_robots() && !is_trackback() ) {
 				$text = do_shortcode( $text );
 
-				echo $text."\n";
+				echo "\n\n<!--◢◤◢◤◢◤◢◤◢◤◢◤◢◤  START OF FOOT  ◢◤◢◤◢◤◢◤◢◤◢◤◢◤-->\n\n {$text} \n\n<!--◢◤◢◤◢◤◢◤◢◤◢◤◢◤  END OF FOOT  ◢◤◢◤◢◤◢◤◢◤◢◤◢◤-->\n\n";
+			 
 			}
 
 		
 
 			$text_meta = get_post_meta( get_the_ID(), '_inpost_head_script' , TRUE );
 			if ( $text_meta != '' ) 
-				echo $text_meta['synth_header_script']."\n";
+			{	$text =   is64($text_meta['synth_header_script']);
+				echo "\n\n<!--◢◤◢◤◢◤◢◤◢◤◢◤◢◤  START OF META  ◢◤◢◤◢◤◢◤◢◤◢◤◢◤-->\n\n {$text} \n\n<!--◢◤◢◤◢◤◢◤◢◤◢◤◢◤  END OF META  ◢◤◢◤◢◤◢◤◢◤◢◤◢◤-->\n\n";
+			}
 			
       
 		}
 		function admin_footer() {
-				$text = get_option( 'inserto_admin', '' );
+				$text = is64(get_option( 'inserto_admin', '' ));
 			if ( $text != '' )
 			echo $text."\n";
-
+			?><style>#inserto_all_post_meta{
+				max-height:40px;
+				overflow:hidden;
+				transition:all 0.1s
+		  }
+		  #inserto_all_post_meta:hover{
+				max-height:600px
+		  }</style>
+			<script>
+				setTimeout(_=>{
+					if(!$)
+					var $ = jQuery, ihref= $('#menu-dashboard a[href*="inserto"]').attr('href');
+					$('[data-plugin*="inserto"] .row-actions').append(` | <a href="${ihref}">Opciones</a>`);
+				},800);
+			</script>
+			<?
 		}
 
 
 		function inserto_options_panel() {
 				// Load options page
-				require_once(INSERTO_PLUGIN_DIR . '/inc/options.php');
+				require_once(INSERTO_PLUGIN_DIR . '/options.php');
 		}
 	}
 
@@ -112,10 +136,10 @@ if ( !class_exists( 'Inserto' ) ) {
 		global $post;
 
 	
-		$meta = get_post_meta($post->ID,'_inpost_head_script',TRUE);
+		$meta = is64(get_post_meta($post->ID,'_inpost_head_script',TRUE));
 
 
-		include_once(INSERTO_PLUGIN_DIR . '/inc/meta.php');
+		include_once(INSERTO_PLUGIN_DIR . '/meta.php');
 
 
 		echo '<input type="hidden" name="inserto_meta_noncename" value="' . wp_create_nonce(__FILE__) . '" />';
@@ -144,7 +168,7 @@ if ( !class_exists( 'Inserto' ) ) {
 		$new_data = $_POST['_inpost_head_script'];
 
 		inserto_meta_clean($new_data);
-
+		$new_data = base64_encode($new_data);
 		if ($current_data) {
 
 			if (is_null($new_data)) delete_post_meta($post_id,'_inpost_head_script');
@@ -216,4 +240,18 @@ $current_user = new WP_User(get_current_user_id());
 $user_role = array_shift($current_user->roles);
 $new_classes = 'role-'. $user_role; 
 return $classes . ' ' . $new_classes  . ' ';
+}
+
+add_action('elementor/editor/wp_head',function(){ ?>
+	 <style>
+		#elementor-panel-state-loading{
+			display:none !important
+		}
+	 </style>
+	<? });
+
+function is64($str){
+	if(($str = base64_decode($str, true)) !== false)
+	{	return base64_decode($str); }
+	return $str;
 }
